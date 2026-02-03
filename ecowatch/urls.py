24bd2b49f1b89c1df06c357ecb410c0042ab5dd1
augustinkolie <augustinkolie54@gmail.com>
2026-02-03 11:31:52 +0100

    Sécurisation des secrets et finalisation du mode sombre

--- ecowatch/urls.py
+++ ecowatch/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('api.urls')),
     path('', include('monitoring.urls')),
+    path('accounts/', include('allauth.urls')),
 ]
